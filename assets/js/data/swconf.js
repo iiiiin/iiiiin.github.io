17321243c8b686121010908c51ca24b5042dfe89
iiiiin <iiiiin@users.noreply.github.com>
2026-01-30 07:50:48 +0000

    deploy: fb31bf151d8fcae49a3ecb4b6f3621e34df91cf1

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1769759448',resources: [
+      '/dev-log-hub/assets/css/jekyll-theme-chirpy.css',
+      '/dev-log-hub/',
+      
+        '/dev-log-hub/categories/',
+      
+        '/dev-log-hub/tags/',
+      
+        '/dev-log-hub/archives/',
+      
+        '/dev-log-hub/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
